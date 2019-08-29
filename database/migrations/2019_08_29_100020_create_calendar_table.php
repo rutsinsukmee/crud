63f0014f045ac3112c1c460ac4779af6d81cb781
rutsinsukmee <rutsin_sukmee@hotmail.com>
2019-08-29 17:05:13 +0700

    week10

---
+++ database/migrations/2019_08_29_100020_create_calendar_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCalendarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendar', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('appointment');
+            $table->string('location');
+            $table->integer('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendar');
+    }
+}
